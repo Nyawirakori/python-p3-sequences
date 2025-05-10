df01bc88b78fbcaa756c564f0fae5119941f11af
Joan Nyawira Kori <korijoan171@gmail.com>
2025-05-10 11:45:16 +0300

    Fibonacci sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(length): 
+    sequence = []
+    a,b = 0,1 # two "boxes" a and b that have values 0 and 1 to start us off
+    #create a for loop to loop through the length
+    for _ in range(length):
+        sequence.append(a)#append at the end of the list the new value of a
+        a,b = b,a+b
+      #print the new values in the sequence
+    print(sequence)
+
+
